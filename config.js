9831fdfa0bdeb27f818176bb0ae4990c4e94015c
PhilipAB <philip231001@googlemail.com>
2020-07-28 06:50:33 +1000

    initial commit with test config

---
+++ config.js
@@ -0,0 +1,490 @@
+const dotenv = require('dotenv').config({ path: 'process.env' });
+
+var config = {};
+
+config.lunchContractABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "friendAddress",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            }
+        ],
+        "name": "addFriend",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "label",
+                "type": "string"
+            }
+        ],
+        "name": "addVenue",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "venue",
+                "type": "uint256"
+            }
+        ],
+        "name": "doVote",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "validVote",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "finalVenue",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "friends",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "internalType": "bool",
+                "name": "voted",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numFriends",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numVenues",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numVotes",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "results",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "venues",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "votes",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "voterAddress",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "venue",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+config.lunchExtendedContractABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "friendAddress",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            }
+        ],
+        "name": "addFriend",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "label",
+                "type": "string"
+            }
+        ],
+        "name": "addVenue",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "cancelLunch",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "createVoting",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "venue",
+                "type": "uint256"
+            }
+        ],
+        "name": "doVote",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "validVote",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "finalVenue",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "friends",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "internalType": "bool",
+                "name": "voted",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numFriends",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numVenues",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numVotes",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "results",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "retrieveContract",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "venues",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "votes",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "voterAddress",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "venue",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+config.masterAddress = process.env.MASTERADDRESS;
+config.contractAddressLunch = process.env.LUNCHCONTRACTADDRESS;
+config.contractAddressLunchExtended = process.env.LUNCHEXTENDEDCONTRACTADDRESS;
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,60 @@
+const web3 = require('web3');
+const express = require('express');
+const Tx = require('ethereumjs-tx');
+const config = require('./config');
+
+const app = express();
+
+//Infura HttpProvider Endpoint
+web3js = new web3(new web3.providers.HttpProvider("https://ropsten.infura.io/v3/674c8c3bc3c64cc6bd6ce5849f05219c"));
+
+app.get("/test", function (request, response) {
+    response.send({
+        test: 150
+    })
+});
+
+app.get('/numVoteTest', async function (req, res) {
+
+    var contract = new web3js.eth.Contract(config.lunchContractABI, config.contractAddressLunch);
+
+    var returnValue = await contract.methods.numVotes().call({ from: config.contractAddressLunch });
+
+    var gasPrice = await web3js.eth.getGasPrice(function (e, r) { console.log('Error: ', e) });
+
+    var latestBlock = await web3js.eth.getBlock("latest");
+
+    var rawTransaction = {
+        "from": config.masterAddress,
+        "gasPrice": gasPrice,
+        "gasLimit": latestBlock.gasLimit,
+        "to": config.contractAddressLunch,
+        "data": contract.methods.numVotes().encodeABI(),
+        "return value": returnValue
+    }
+    res.send({ rawTransaction });
+})
+
+app.get('/numVote', async function (req, res) {
+
+    //creating contract object
+    var contract = new web3js.eth.Contract(config.lunchExtendedContractABI, config.contractAddressLunchExtended);
+
+    var returnValue = await contract.methods.numVotes().call({ from: config.contractAddressLunchExtended });
+
+    var gasPrice = await web3js.eth.getGasPrice(function (e, r) { console.log('Error: ', e) });
+
+    var latestBlock = await web3js.eth.getBlock("latest");
+
+    var rawTransaction = {
+        "from": config.masterAddress,
+        "gasPrice": gasPrice,
+        "gasLimit": latestBlock.gasLimit,
+        "to": config.contractAddressLunch,
+        "data": contract.methods.numVotes().encodeABI(),
+        "return value": returnValue
+    }
+    res.send({ rawTransaction });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
