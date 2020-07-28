b47056b10aa5b1d13092383e030cbe55c1ce8064
PhilipAB <philip231001@googlemail.com>
2020-07-28 23:33:17 +1000

    minor fix (2) to suppress warning in case no process is active

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,3 @@
 source /home/ec2-user/.bash_profile
 sudo rm -rf /home/ec2-user/raw
-sudo -H -u ec2-user bash -c '/home/ec2-user/.nvm/versions/node/v14.6.0/bin/pm2 delete all' 2> /dev/null
+sudo -H -u ec2-user bash -c '/home/ec2-user/.nvm/versions/node/v14.6.0/bin/pm2 delete all --silent'
