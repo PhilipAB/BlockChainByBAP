0c6ee7303a00933d9ec597702563808fd287077d
PhilipAB <philip231001@googlemail.com>
2020-07-28 21:19:59 +1000

    adjustments for production mode

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,4 +1,4 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/BlockChainByBAP
-sudo -H -u ec2-user bash -c '/home/ec2-user/.nvm/versions/node/v14.6.0/bin/npm start'
+sudo -H -u ec2-user bash -c 'pm2 start index.js'
 
