b15da265ad052401883de9d1979a74a96edac95d
PhilipAB <philip231001@googlemail.com>
2020-07-31 10:45:19 +1000

    fix body parsing for post requests

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Infura HttpProvider Endpoint
 //web3js.currentProvider.disconnect();
